78e1756e0b1fccbdfc01f85d0fcbdce0583034e1
Marina Kraynova <kraynovamg@gmail.com>
2022-04-29 15:07:09 +0300

    add Conflict error at updateUserInfo

--- controllers/users.js
+++ controllers/users.js
@@ -30,26 +30,37 @@ module.exports.getUserInfo = (req, res, next) => {
 
 module.exports.updateUserInfo = (req, res, next) => {
   const { name, email } = req.body;
-  User.findByIdAndUpdate(
-    req.user._id,
-    { name, email },
-    { new: true, runValidators: true },
-  )
-    .then((user) => {
-      if (user) {
-        res.send(user);
+
+  User.find({ email })
+    .then((result) => {
+      if (result.length === 0) {
+        User.findByIdAndUpdate(
+          req.user._id,
+          { name, email },
+          { new: true, runValidators: true },
+        )
+          .then((user) => {
+            if (user) {
+              res.send(user);
+            } else {
+              next(new NotFoundError('Ошибка. Пользователь не найден, попробуйте еще раз'));
+            }
+          })
+          .catch((err) => {
+            if (err.name === 'CastError') {
+              next(new CastError('Ошибка. Введен некорректный id пользователя'));
+            } else if (err.name === 'ValidationError') {
+              next(new ValidationError('Ошибка. При обновлении данных пользователя были переданы некорректные данные'));
+            } else {
+              next(err);
+            }
+          });
       } else {
-        next(new NotFoundError('Ошибка. Пользователь не найден, попробуйте еще раз'));
+        next(new ConflictingRequest('Ошибка. Пользователь c таким email уже зарегистрирован'));
       }
     })
     .catch((err) => {
-      if (err.name === 'CastError') {
-        next(new CastError('Ошибка. Введен некорректный id пользователя'));
-      } else if (err.name === 'ValidationError') {
-        next(new ValidationError('Ошибка. При обновлении данных пользователя были переданы некорректные данные'));
-      } else {
-        next(err);
-      }
+      next(err);
     });
 };
 
