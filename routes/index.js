3f9484649e38ff7f5423658681c13528238b83f5
Marina Kraynova <kraynovamg@gmail.com>
2022-04-29 15:39:27 +0300

    handle not found route

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const auth = require('../middlewares/auth');
 const routerAuthorization = require('./authorization');
 const routerUsers = require('./users');
 const routerMovies = require('./movies');
+const NotFoundError = require('../errors/NotFoundError');
 
 routerApp.use('/api', routerAuthorization);
 
@@ -11,5 +12,9 @@ routerApp.use(auth);
 routerApp.use('/api/users', routerUsers);
 
 routerApp.use('/api/movies', routerMovies);
+
+routerApp.use((req, res, next) => {
+  next(new NotFoundError('Ошибка. Роут не существует'));
+});
 
 module.exports = routerApp;
